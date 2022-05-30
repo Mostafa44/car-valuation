cd81ac618eb8593be194537ba9ce11e037632a91
mostafa.abdelshafy <mostafa.abdelshafy88@gmail.com>
2022-05-30 09:17:23 +0200

    Serialization in interceptor

--- src/interceptors/serializer.interceptior.ts
+++ src/interceptors/serializer.interceptior.ts
@@ -2,16 +2,18 @@ import { ExecutionContext, CallHandler, UseInterceptors, NestInterceptor } from 
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 import { plainToClass } from "class-transformer";
+import { UserDto } from "src/users/dtos/user.dto";
 
 
 export class SerializeInterceptor implements NestInterceptor{
     intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any>  {
-       //this is going to run before the handler call is made
-       console.log('1- before the handler', context);
+    
 
        //this is going to run before a response is being sent back
         return next.handle().pipe(map((data: any)=>{
-            console.log('3- before the response ', data);
+           return  plainToClass(UserDto, data, {
+               excludeExtraneousValues:true
+           })
         })
         );
     }
